9ccbc340a66a1411e391464e8b4a4fb7424b30cc
shashank <shashankbandari777@gmail.com>
2025-03-17 18:40:36 +0530

    30

--- task04/main.tf
+++ task04/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_public_ip" "main" {
   name                = var.public_ip_name
   location            = var.location
   resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   domain_name_label   = var.dns_name_label
   tags                = var.tags
 }
